eefb3638849ee67c695fa62c76cbf231f2e0946e
shad0wshayd3 <shad0wshayd3@users.noreply.github.com>
2024-11-20 22:01:42 -0700

    refactor: remove figcone

--- xmake.lua
+++ xmake.lua
@@ -4,12 +4,9 @@ set_xmakever("2.9.4")
 -- include local folders
 includes("extern/commonlibsse")
 
--- add custom package repository
-add_repositories("xre https://github.com/shad0wshayd3/custom-xrepo")
-
 -- set project
 set_project("BakaWorldMapFOV")
-set_version("1.0.0")
+set_version("2.0.0")
 set_license("GPL-3.0")
 
 -- set defaults
@@ -26,19 +23,14 @@ set_policy("build.optimization.lto", true)
 add_rules("mode.debug", "mode.releasedbg")
 add_rules("plugin.vsxmake.autoupdate")
 
--- compile for ae
+-- add config
+set_config("rex_ini", true)
 set_config("skyrim_ae", true)
-
--- require package dependencies
-add_requires("figcone", {configs = {use_ini = true}})
 
 -- setup targets
 target("BakaWorldMapFOV")
     -- bind local dependencies
     add_deps("commonlibsse")
-
-    -- bind package dependencies
-    add_packages("figcone")
 
     -- add commonlibsse plugin
     add_rules("commonlibsse.plugin", {
@@ -47,8 +39,8 @@ target("BakaWorldMapFOV")
     })
 
     -- add source files
-    add_files("src/*.cpp")
-    add_headerfiles("src/*.h")
+    add_files("src/**.cpp")
+    add_headerfiles("src/**.h")
     add_includedirs("src")
     set_pcxxheader("src/PCH.h")
 
